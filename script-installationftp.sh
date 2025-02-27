3075ca028f924cea5d7c15134f3ed3f00027be9b
IlyesBenhammou <ia.benhammou@gmail.com>
2025-02-27 08:37:13 +0100

    Script installation simple V.0

--- script-installationftp.sh
+++ script-installationftp.sh
@@ -1,1 +1,17 @@
 # Script Pour l'installation du serveur FTP
+
+#!/bin/bash
+
+# Vérifier les privilèges root
+if [ "$EUID" -ne 0 ]; then
+  echo "Veuillez exécuter ce script en tant que root."
+  exit 1
+fi
+
+# Mettre à jour les dépôts
+echo "Mise à jour des dépôts..."
+apt update -y
+
+# Installer Pure-FTPd
+echo "Installation de Pure-FTPd..."
+apt install -y pure-ftpd
